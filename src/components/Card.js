36e15bc52ef73486e29ddab7a8c8b6a5ba5868a5
dijair12 <dijair.ribeiro@aluno.faculdadeimpacta.com.br>
2021-05-01 17:25:54 -0300

    refactoring

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ const Card = ({
   email
 }) => {
   return (
-    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-S">
+    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-S" style={{ width: '19rem', fontSize: '0.7rem' }}>
       <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
       <div>
         <h2>{name}</h2>
